23ca8563b03a3a1e659c0baf6ddced4242e55742
XenonTheInertG <XenonTheInertG@outlook.com>
2022-04-04 19:19:14 +0600

    TWRP-CI: Initial Commit

---
+++ config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Device
+export TWRP_MANIFEST ="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
+export TWRP_BRANCH ="twrp-11"
+export DT_LINK="https://github.com/XenonTheInertG-CI/android_device_oneplus_OnePlus9Pro -b a11"
+
+export DEVICE="beryllium"
+export OEM="xiaomi"
+export TARGET="recoveryimage"
+
+export OUTPUT="TWRP*.zip"
+
+# Kernel Source
+# Uncomment the next line if you want to clone a kernel source.
+#export KERNEL_SOURCE=""
+
+# Extra Command
+export EXTRA_CMD=""
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path.
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
+
+if [ ! -z "$PLATFORM" ]; then
+    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+else
+    export KERNEL_PATH="kernel/$OEM/$DEVICE"
+fi
+export DT_PATH="device/$OEM/$DEVICE"
